419a05c98962a31fcd5ed6bcc277930b1b3bf08f
Daisuke <dk.joker.1107@icloud.com[credential]>
2021-06-18 12:48:40 -0700

    add about me section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/header/Header';
 import Work from './components/work/Work';
+import AboutMe from './components/aboutMe/AboutMe';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Work/>
+      <AboutMe/>
     </div>
   );
 }
---
+++ src/components/aboutMe/AboutMe.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./aboutMe.css";
+
+export default function AboutMe() {
+    return (
+        <div id="about-me">
+            <div className="about-me-wrapper">
+                <h3 className="section-title">About Me</h3>
+                <hr className="section-title-underline" />
+                <div className="about-me-main">
+                    <div className="about-me-left">
+                        <img src="/avatar.png" alt="" className="about-img" />
+                    </div>
+                    <div className="about-me-right">
+                        <p className="about-me-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam augue ipsum, sagittis et mollis eget, scelerisque fringilla ipsum. Etiam nec libero nisi. Duis condimentum felis sit amet sollicitudin hendrerit. Donec dapibus ex vitae ex efficitur, finibus ultricies dui cursus. Sed laoreet, enim eleifend dictum commodo, magna eros suscipit enim, eu scelerisque lacus lacus a nibh. Vivamus at ligula in ligula fringilla euismod. Proin vel massa ex. Sed pharetra tempor nisl vitae malesuada. Mauris ac eros augue. Aliquam cursus purus ipsum, quis cursus libero laoreet vitae. Curabitur cursus ligula eget lacinia facilisis.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
