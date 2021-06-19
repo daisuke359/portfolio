52d12f361985ac26e1d5be4c33371a548844b0a9
Daisuke <dk.joker.1107@icloud.com[credential]>
2021-06-19 13:53:07 -0700

    fix css

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./footer.css";
 export default function Footer() {
     return (
         <footer>
-            <p><span>&copy;</span> 20121 Daisuke Kawabuchi</p> 
+            <p><span>&copy;</span> 2021 Daisuke Kawabuchi</p> 
         </footer>
     )
 }
--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -10,10 +10,10 @@ export default function Work() {
                 <p className="section-desc">Here are the few projects I've worked on recently.</p>
                 <div className="work-list">
                     <div className="work-item">
-                    <a href="https://tender-benz-7f5f5b.netlify.app/" target="_blank" rel="noreferrer" ><img className="app-img" src="/social-app.png" alt="" /></a>
+                    <a href="https://social-app-mern.netlify.app" target="_blank" rel="noreferrer" ><img className="app-img" src="/social-app.png" alt="" /></a>
                     </div>
                     <div className="work-item">
-                    <a href="https://suspicious-einstein-fad332.netlify.app/" target="_blank" rel="noreferrer" ><img className="app-img" src="./twitter-app.png" alt="" /></a>
+                    <a href="https://twitter-api-search.netlify.app" target="_blank" rel="noreferrer" ><img className="app-img" src="./twitter-app.png" alt="" /></a>
                     </div>
                 </div>
                 <a href="https://github.com/daisuke359" target="_blank" rel="noreferrer" ><button className="work-btn">See more on GitHub</button></a>
