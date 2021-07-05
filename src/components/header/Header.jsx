82fefea3063955b6df306f9382f0e60e0521c57a
Daisuke <dk.joker.1107@icloud.com[credential]>
2021-07-05 16:51:36 -0700

    add animation to header text

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import "./header.css";
 import Navbar from '../Nav/Navbar';
+import Typical from "react-typical";
 
 export default function Header() {
     return (
         <header>
           <Navbar/>
-        {/* <div className="header-nav">
-          <div className="logo-container">
-            <i className="fab fa-github"></i>
-          </div>
-          
-          <ul className="nav-list">
-            <li className="nav-item"><a href="#work">Work</a></li>
-            <li className="nav-item"><a href="#about-me">About Me</a></li>
-            <li className="nav-item"><a href="#skill">Skill</a></li>
-            <li className="nav-item"><a href="#contact">Contact</a></li>
-          </ul>
-          <i className="fas fa-bars nav-icon"></i>
-        </div> */}
         <div className="header-main">
           <h2 className="header-title">Hi, I'm Daisuke.</h2>
-          <h4 className="header-desc">I'm a front end engineer.</h4>
+          <h4 className="header-desc">
+            <Typical 
+              loop={Infinity}
+              wrapper="b"
+              steps={[
+                'Hello 👋',
+                1000,
+                'I am a front end engineer 👨‍💻',
+                1000
+              ]}
+            /></h4>
             <a href="#work"><button className="header-btn">See my work</button></a>
             <a href="#contact"><button className="header-btn">Get in touch</button></a>
         </div>
