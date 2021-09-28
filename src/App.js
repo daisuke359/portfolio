34214d6b2d67ad72b84eb39879a26c86dbec88c2
Daisuke <dk.joker.1107@icloud.com[credential]>
2021-09-28 15:15:05 -0700

    update about me section

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import './App.css';
 import Header from './components/header/Header';
 import Work from './components/work/Work';
 import AboutMe from './components/aboutMe/AboutMe';
-import Skill from './components/skill/Skill';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar/> */}
       <Header/>
-      {/* <AboutMe/> */}
+      <AboutMe/>
       <Work/>
-      {/* <Skill/> */}
       <Contact />
       <Footer />
     </div>
--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -12,16 +12,30 @@ export default function AboutMe() {
     return (
         <section className="about-me" id="about-me">
             <div className="about-me-wrapper">
-                <h3 className="section-title">About Me</h3>
+                <h3 className="section-title">ABOUT ME</h3>
                 <hr className="section-title-underline" />
                 <div className="about-me-main">
                     <div data-aos="fade-right" className="about-me-left">
-                        <img src="/avatar.png" alt="" className="about-img" />
+                        <h4 className="work-title">Get to know me!</h4>
+                        <p className="about-me-desc">My name is Daisuke. I am a <span>Front-End Web developer</span> based in Vancouver! I am passionate about creating interactive web applicaitons and engaging user experience using React.</p>
+                        <p className="about-me-desc">I recently graduated from a Douglas College with diploma degree in <span>Computer Studies an Information Systems💻</span></p>
+                        <p className="about-me-desc">Other than coding, I spend most of my time exploring the city of Vancouver, shooting photos and videos, and trying out new restaurants around the city! And I'm always down for Ramen🍜</p>
                     </div>
                     <div data-aos="fade-left" className="about-me-right">
-                        <p className="about-me-desc">My name is Daisuke. I am a Front-End Web developer based in Vancouver! I am passionate about creating interactive web applicaitons and engaging user experience using React.</p>
-                        <p className="about-me-desc">I am currently working towards a diploma degree in Computer Studies an Information Systems at Douglas College.💻</p>
-                        <p className="about-me-desc">Other than coding, I spend most of my time exploring the city of Vancouver, shooting photos and videos, and trying out new restaurants around the city! And I'm always down for Ramen🍜</p>
+                        <h4 className="work-title">My Skills</h4>
+                        <ul className="my-skills">
+                            <li className="my-skill">HTML</li>
+                            <li className="my-skill">CSS</li>
+                            <li className="my-skill">JAVASCRIPT</li>
+                            <li className="my-skill">RESPONSIVE DESIGN</li>
+                            <li className="my-skill">REACT</li>
+                            <li className="my-skill">NODE</li>
+                            <li className="my-skill">EXPRESS</li>
+                            <li className="my-skill">MONGO DB</li>
+                            <li className="my-skill">CONTEXT API</li>
+                            <li className="my-skill">GIT</li>
+                            <li className="my-skill">GITHUB</li>
+                        </ul>
                     </div>
                 </div>
             </div>
